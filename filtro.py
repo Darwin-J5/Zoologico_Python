8a0747242d3757cf56afa5934efc28af0b5cfe66
Darwin-J5 <darwin.jimbo@unl.edu.ec>
2024-12-13 10:18:25 -0500

    Create filtro.py

---
+++ filtro.py
@@ -0,0 +1,12 @@
+import django_filters
+from .models import Animal
+
+class AnimalFilter(django_filters.FilterSet):
+    class Meta:
+        model = Animal
+        fields = {
+            'nombre': ['icontains'],
+            'especie': ['icontains'],
+            'edad': ['gte', 'lte'],
+            'habitat__nombre': ['icontains'],
+        }
